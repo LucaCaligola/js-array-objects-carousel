ba4c72e7728a60fee8ce3c27b6b7be2ad5497347
LucaCaligola <luca.caligola@gmail.com>
2023-11-27 17:00:00 +0100

    stuck with an array problem

--- script.js
+++ script.js
@@ -14,24 +14,28 @@
 // l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per 
 // l'ultima miniatura se l'utente clicca la freccia verso il basso.
 
-const images = [
+const pic = [
     {
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
+    }, 
+    {
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
+    }, 
+    {
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
+    }, 
+    {
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
+    }, 
+    {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -44,7 +48,9 @@ const slideContainer = document.getElementsByClassName('.container');
 const slideText = document.querySelector('p')
 const img = document.querySelector('img')
 
+console.log(pic.image)
 
-img.src += `${images.image}`
+
+// img.src += `${images.image}`
 
 // nextButton.addEventListener('click')
